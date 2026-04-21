49ac923299f951cd8c32a31a2160fcf7d4baae87
ashab-al <asxabal7@gmail.com>
2026-04-21 17:26:23 +0300

    fix/fix

--- app/query_objects/tasks/find_task_by_id.py
+++ app/query_objects/tasks/find_task_by_id.py
@@ -1,7 +1,7 @@
 from models.task import Task
 
 def find_task_by_id(id: int, db: list[Task]) -> Task | None:
-    for task in (task for task in db if task.id == id):
+    for task in db:
         if task.id == id:
             return task
 
