3b3f94da5a15b2a4ed9791c8a1fbdf275cbeea45
ashab-al <asxabal7@gmail.com>
2026-04-21 16:48:26 +0300

    find_all_tasks

--- app/api/tasks/list.py
+++ app/api/tasks/list.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
 from schemas.api.tasks.list.response import TaskListResponse
+from services.tasks.find_all_tasks import find_all_tasks
+from database import Task, get_database
 
 
 router = APIRouter()
@@ -10,5 +14,12 @@ router = APIRouter()
     description="Возвращает все таски.",
     response_model=list[TaskListResponse]
 )
-async def list_task():
-    ...
+async def list_task(
+    db: Annotated[list[Task], Depends(get_database)]
+):
+    try:
+        tasks = await find_all_tasks(db)
+    except Exception as e:
+        raise HTTPException(400, str(e)) from e
+
+    return tasks
