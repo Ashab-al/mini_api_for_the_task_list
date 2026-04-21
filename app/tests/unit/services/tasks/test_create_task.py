79f406d4c6b599595cca8cb0b56ce199c8f58acc
ashab-al <asxabal7@gmail.com>
2026-04-21 17:22:50 +0300

    fix/tests

--- app/tests/unit/services/tasks/test_create_task.py
+++ app/tests/unit/services/tasks/test_create_task.py
@@ -5,7 +5,6 @@ from services.tasks.create_task import create_task
 
 
 def test_create_task():
-    # Arrange
     title = "Test Title"
     description = "Test Description"
     status = "new"
@@ -16,20 +15,16 @@ def test_create_task():
         status=status
     )
 
-    # Имитируем базу данных как список задач
     mock_db = [
         Mock(id=1, title="Existing Task", description="Old", status="done"),
     ]
 
-    # Act
     new_task = create_task(request, mock_db)
 
-    # Assert
     assert new_task.id == 2
     assert new_task.title == title
     assert new_task.description == description
     assert new_task.status == status
 
-    # Проверяем, что задача добавлена в БД
     assert new_task in mock_db
     assert len(mock_db) == 2
